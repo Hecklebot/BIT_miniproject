6e6eb8c6a934e0ee982a83969e7d5c2f93259cec
Hecklebot <51806574+Hecklebot@users.noreply.github.com>
2019-06-20 18:21:37 +0900

    utf-8 check

--- src/Mini_project/Attend.java
+++ src/Mini_project/Attend.java
@@ -4,7 +4,8 @@ public class Attend {
 
     public static void main(String[] args) {
         System.out.println("ぞしぞし");
-
+        System.out.println("abc");
+        System.out.println("馬馬馬");
     }
 
 }
